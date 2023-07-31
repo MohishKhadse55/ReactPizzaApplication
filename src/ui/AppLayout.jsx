e99a39466cf9e88717dc1bfb3dbb70e63d888f0e
Mohish Khadse <mohishkhadse3222@gmail.com>
2023-07-31 22:48:20 +0530

    Initial commit

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import Header from "./Header.jsx";
-import CartOverview from "../features/cart/CartOverview.jsx";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader.jsx";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
-function AppLayout(props) {
+function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
+
       <Header />
-      <main>
-        <h1>Cart Content</h1>
-        <Outlet />
-      </main>
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
       <CartOverview />
     </div>
   );
